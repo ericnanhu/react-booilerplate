7a9facb6679078bc8d6f454bf38bec2951ca05b9
Eric Hu <huericnan@gmail.com>
2024-02-01 16:19:58 +0800

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,39 +8,55 @@ export default async function Home() {
   const session = await getServerSession(authOptions);
 
   return (
-    <div className="grid grid-cols-12">
-      <div className="col-span-3"></div>
-      <div className="col-span-9">
-        Main content
-        <div>
-          <Button color="primary" loading={true}>
-            Button
-          </Button>
-          <Button color="primary-bordered" loading={true}>
-            Button
-          </Button>
-          <Button color="danger" loading={true}>
-            Button
-          </Button>
-          <Button color="danger-bordered" loading={true}>
-            Button
-          </Button>
-          <Button color="primary" loading={false}>
-            Button
-          </Button>
-          <Button color="primary-bordered" loading={false}>
-            Button
-          </Button>
-          <Button color="danger" loading={false}>
-            Button
-          </Button>
-          <Button color="danger-bordered" loading={false}>
-            Button
-          </Button>
-        </div>
-        <div>
-          <Notification type="success" message="lorem ipsum" />
-        </div>
+    <div className="h-screen flex flex-col content">
+      <p>
+        This is a SaaS boilerplate created using Next.js. The boilerplate comes
+        with:
+      </p>
+
+      <ul>
+        <li>A user sign in/sign up system, created with Auth.js.</li>
+        <li>A payment system, integrated with Stripe.</li>
+        <li>Email system, integrated with SendGrid.</li>
+        <li>A community blog system, based on Tina CMS.</li>
+        <li>
+          Several UI components, such as buttons, form inputs, cards,
+          notifications, and more.
+        </li>
+      </ul>
+
+      <p>More features and UI components coming soon.</p>
+
+      <h2>Buttons</h2>
+
+      <div className="flex flex-row gap-2">
+        <Button color="primary" loading={true}>
+          Button
+        </Button>
+        <Button color="primary-bordered" loading={true}>
+          Button
+        </Button>
+        <Button color="danger" loading={true}>
+          Button
+        </Button>
+        <Button color="danger-bordered" loading={true}>
+          Button
+        </Button>
+        <Button color="primary" loading={false}>
+          Button
+        </Button>
+        <Button color="primary-bordered" loading={false}>
+          Button
+        </Button>
+        <Button color="danger" loading={false}>
+          Button
+        </Button>
+        <Button color="danger-bordered" loading={false}>
+          Button
+        </Button>
+      </div>
+      <div>
+        <Notification type="success" message="lorem ipsum" />
       </div>
     </div>
   );
